7100d65566697aee4d4b72501e74731eacf1ed71
Your Name <you@example.com>
2021-12-13 19:25:49 +0200

    syksy loppu

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -22,7 +22,27 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // haetaan näppäimistöltä sivusuunnan liikkeen input
         horizontalMovement = Input.GetAxis("Horizontal");
-        Debug.Write(horizontalMovement);
+        // Debug.Log(horizontalMovement);
+        // käännetään pelaaja menosuuntaan
+        float facing = Input.GetAxisRaw("Horizontal");
+        if (facing != 0)
+        {
+            transform.localScale = new Vector2(facing, transform.localScale.y);
+        }
     }
+
+    // FixedUpdate kutsutaan tietyin väliajoin
+    private void FixedUpdate()
+    {
+        // haetaan mikä on oikea liikemäärä sivusuunnassa
+        float xMovement = horizontalMovement * moveSpeed;
+        // pystysuunnan liikkeeseen otetaan nykyinen liike
+        float yMovement = myRigidbody.velocity.y;
+        // tehdään näistä vektori
+        Vector2 movementVector = new Vector2(xMovement, yMovement);
+        // asetetaan tämä rigidbodyn liikevektoriksi
+        myRigidbody.velocity = movementVector;
+    } 
 }
